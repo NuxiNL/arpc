ea2c46eafcd4de6efaddbcab81771e0e8ca88776
Ed Schouten <ed@nuxi.nl>
2017-06-13 16:01:10 +0200

    Use EXPECT_EQ() instead of ASSERT_EQ().

--- src/server_test.cc
+++ src/server_test.cc
@@ -9,16 +9,16 @@
 TEST(Server, BadFileDescriptor) {
   // Attempting to use a bad file descriptor should trigger EBADF.
   arpc::ServerBuilder builder(-1);
-  ASSERT_EQ(EBADF, builder.Build()->HandleRequest());
+  EXPECT_EQ(EBADF, builder.Build()->HandleRequest());
 }
 
 TEST(Server, EndOfFile) {
   // Close one half of a socket pair. Reading requests should return
   // end-of-file, which is encoded as -1.
   int fds[2];
-  ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
-  ASSERT_EQ(0, close(fds[0]));
+  EXPECT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
+  EXPECT_EQ(0, close(fds[0]));
   arpc::ServerBuilder builder(fds[1]);
-  ASSERT_EQ(-1, builder.Build()->HandleRequest());
-  ASSERT_EQ(0, close(fds[1]));
+  EXPECT_EQ(-1, builder.Build()->HandleRequest());
+  EXPECT_EQ(0, close(fds[1]));
 }
