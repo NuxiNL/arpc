dbc32e6706e4eb64e5ff4fc855e533d039da2243
Ed Schouten <ed@nuxi.nl>
2017-06-13 15:51:08 +0200

    Start adding some simple unit tests.

---
+++ src/server_test.cc
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <arpc++/arpc++.h>
+#include <gtest/gtest.h>
+
+TEST(Server, BadFileDescriptor) {
+  // Attempting to use a bad file descriptor should trigger EBADF.
+  arpc::ServerBuilder builder(-1);
+  ASSERT_EQ(EBADF, builder.Build()->HandleRequest());
+}
+
+TEST(Server, EndOfFile) {
+  // Close one half of a socket pair. Reading requests should return
+  // end-of-file, which is encoded as -1.
+  int fds[2];
+  ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
+  ASSERT_EQ(0, close(fds[0]));
+  arpc::ServerBuilder builder(fds[1]);
+  ASSERT_EQ(-1, builder.Build()->HandleRequest());
+  ASSERT_EQ(0, close(fds[1]));
+}
