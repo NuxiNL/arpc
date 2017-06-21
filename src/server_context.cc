a8fd01fdd99c2404ee43a954856e92e69d4d9b4c
Ed Schouten <ed@nuxi.nl>
2017-06-21 05:25:25 +0200

    Add licensing information.

--- src/server_context.cc
+++ src/server_context.cc
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Nuxi (https://nuxi.nl/) and contributors.
+//
+// This file is distributed under a 2-clause BSD license.
+// See the LICENSE file for details.
+
 #include <arpc++/arpc++.h>
 
 using namespace arpc;
